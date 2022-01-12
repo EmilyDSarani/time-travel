dc2914f41fe20e8fd30b349fb45b13268ad36095
EmilyDSarani <emily.sarani@gmail.com>
2022-01-12 17:33:31 -0600

    I can see the date

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,24 @@
-import React from 'react'
-
-
-
+import { useState } from 'react'
 
 export default function App() {
-  
+  const [date, setDate] = useState('');
+  const [undo, setUndo] = useState('');
+  const [redo, setRedo] = useState(''); 
+  const [didNumberChange, setDidNumberChange] = useState('')
 
+ 
+//ideally, I would like this to take in the name and set the date to the value and throw an error if nothing has been selected
+//I know I will need to set the date to the value
 
-
+  const handleSelect =(e) => {
+    const { name, value } = e.target;
+    if (name){
+      setDate(value)
+    } else {
+      throw new Error('Select a date')
+    }
+    
+  }
 
   return (
     <div>
@@ -16,9 +27,10 @@ export default function App() {
       <input 
       type="date"
       name="date"
-      //value= {date}
+      value= {date}
+      onChange={(e) => handleSelect(e)}
       /> 
-      {/* <div> {date} </div> */}
+      <div> {date} </div>
     </div>
   )
 }
