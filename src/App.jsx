9bd2eb6cb86b668e29443608a64369e5df27f58a
EmilyDSarani <emily.sarani@gmail.com>
2022-01-12 18:51:16 -0600

    saving, index is saving

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,56 @@
 import { useState } from 'react'
 
 export default function App() {
-  const [date, setDate] = useState('');
+  const [date, setDate] = useState([]);
+  const [index, setIndex] = useState(-1)
+  const [currentDate, setCurrentDate] = useState('')
   const [undo, setUndo] = useState('');
   const [redo, setRedo] = useState(''); 
   const [didNumberChange, setDidNumberChange] = useState('')
+
+
+  //the first option could be to incriment or decrimate the index and then set the currentDate to that index date[index] (will have to do this for both)
+
+  //a useEffect that takes then set the currentDate to that index date[index] (keeps track once)
 
  
 //ideally, I would like this to take in the name and set the date to the value and throw an error if nothing has been selected
 //I know I will need to set the date to the value
 
-  const handleSelect =(e) => {
-    const { name, value } = e.target;
-    if (name){
-      setDate(value)
-    } else {
-      throw new Error('Select a date')
-    }
+//index/pointer where we are at. the index points at where we should be at depending on what is being displayed
+
+//save the items in an array
+//then add the pointer
+
+  // const handleSelect =(e) => {
+  //   const { name, value } = e.target;
+  //   if (name){
+  //     setDate(value)
+  //   } else {
+  //     throw new Error('Select a date')
+  //   }
     
+  // }
+
+  const handleSaveDate = (e) =>{
+    //setting the name and value to the e.target
+    //pulling off the value from event (which is a date as a string because it is coming from the input field)
+   const {value} = e.target;
+   //set the current date to the value of that target
+   //this finished that controlled input loop, it is making the input works with state.
+   setCurrentDate(value)
+   //setDate to the prev of the name and value
+   //we are taking that same value and popping into the end of this array. We are trying to track what index we are at because that index is how we nav. 
+   setDate((prevState)=>{ return[ ...prevState, value]
+   })
+   //set the index to the length of that date
+   //so we just incriminating the index because we want to be able to follow where it is at
+   setIndex(date.length)
+
+   //not sure if this is needed, because it SHOULD be at 0 and it SHOULD incrimate to 1
+  //  if (index === 0){
+  //    setCurrentDate(date [index]);
+  //  }
   }
 
   return (
@@ -27,10 +60,10 @@ export default function App() {
       <input 
       type="date"
       name="date"
-      value= {date}
-      onChange={(e) => handleSelect(e)}
+      value= {currentDate}
+      onChange={(e) => handleSaveDate(e)}
       /> 
-      <div> {date} </div>
+      <div> {currentDate} </div>
     </div>
   )
 }
